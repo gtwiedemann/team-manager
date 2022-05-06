fb95aef40342b74605c9ac5d002b54c1799e068e
gtwiedemann <gabriel_wiedemann@hotmail.com>
2022-05-06 20:43:51 -0300

    add mock for timesheet

---
+++ src/mock/mock_timesheetData.js
@@ -0,0 +1,46 @@
+let timesheetDate = {
+    'gabriel.wiedemann_2022-05': [
+        {
+            '2022-06-05': [
+                {
+                    un: 'FC3',
+                    activity: 'drawing',
+                    start: '08:00',
+                    finish: '11:00',
+                },
+                {
+                    un: 'FC3',
+                    activity: 'drawing',
+                    start: '11:00',
+                    finish: '12:00',
+                },
+                {
+                    un: 'FC3',
+                    activity: 'report',
+                    start: '12:00',
+                    finish: '17:00',
+                },
+            ],
+            '2022-06-06': [
+                {
+                    un: 'FC3',
+                    activity: 'drawing',
+                    start: '09:00',
+                    finish: '12:00',
+                },
+                {
+                    un: 'FC3',
+                    activity: 'drawing',
+                    start: '12:00',
+                    finish: '15:00',
+                },
+                {
+                    un: 'FC3',
+                    activity: 'report',
+                    start: '15:00',
+                    finish: '19:00',
+                },
+            ],
+        },
+    ],
+};
